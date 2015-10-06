52e0152115ad8f9933477118de50f2f44a3e8603
Bill Rawlinson <bill.rawlinson@sbcs.com>
2015-10-06 09:41:38 -0400

    remove badly named file

--- app/scripts/ng-password-module.js
+++
@@ -1,6 +0,0 @@
-import angular from 'angular';
-
-import NgPasswordStrengthDirective from './ng-password-strength-directive';
-
-export default angular.module('ngPasswordStrength', [])
-  .directive(NgPasswordStrengthDirective.directiveName, NgPasswordStrengthDirective.directiveFactory);
